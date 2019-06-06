16d054d18a34e66979799f80f6ba1b4652f052de
Deepjyoti Mondal <deepjyoti.m@media.net>
2019-06-06 09:37:36 +0530

    Add initial source files

---
+++ saberx/sabercore/shellexecutor.py
@@ -0,0 +1,10 @@
+import subprocess
+
+class ShellExecutor:
+
+	def __init__(self, **kwargs):
+		self.command_list = kwargs.get("command_list")
+		self.logger = kwargs.get("logger")
+
+	def executeShellSingle(self, ):
+		pass
