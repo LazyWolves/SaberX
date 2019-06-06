29f11808c7cb4a0faf61b7734abd4c5ae7d9fc2b
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-06-06 12:41:25 +0530

    Add action group parser

--- saberx/actionparser/actionextractor.py
+++ saberx/actionparser/actionextractor.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 
 class ActionExtractor:
 
@@ -6,10 +7,17 @@ class ActionExtractor:
 		self.configpath = kwargs.get("configpath")
 		self.__load_config()
 		self.__extract_actions()
+		print (json.dumps(self.config, indent=4))
 
-	def __load_config():
+	def __load_config(self):
 		with open(self.configpath) as config:
 			self.config = yaml.load(config)
 
-	def __extract_actions():
-		pass
+	def __extract_action_groups(self):
+		self.action_groups = self.config.get("action_groups")
+
+	def get_action_groups(self):
+		return self.action_groups
+
+if __name__ == "__main__":
+	ActionExtractor(configpath="test.yaml")
