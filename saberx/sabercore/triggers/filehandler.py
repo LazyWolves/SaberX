78e01b9d826da15a6448c7525b70cef7a8910ad5
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-06-06 17:15:11 +0530

    Add empty checking method

--- saberx/sabercore/triggers/filehandler.py
+++ saberx/sabercore/triggers/filehandler.py
@@ -10,4 +10,16 @@ class FileHandler:
         if not os.path.isfile(path):
             return False, "PATH_IS_NOT_A_FILE"
 
-        return True
+        return True, None
+
+    @staticmethod
+    def is_empty(path):
+        is_present, error = FileHandler.is_present(path)
+
+        if not is_present:
+            return False, error
+
+        if os.stat(path).st_size == 0:
+            return True, None
+
+        return False, None
