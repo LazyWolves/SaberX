d3257d08f9e2a6f108d72e2f62264810faf669ad
Deepjyoti Mondal <deepjyoti.m@media.net>
2019-06-06 10:20:52 +0530

    Add method to execute shell

--- saberx/sabercore/shellexecutor.py
+++ saberx/sabercore/shellexecutor.py
@@ -6,5 +6,30 @@ class ShellExecutor:
 		self.command_list = kwargs.get("command_list")
 		self.logger = kwargs.get("logger")
 
-	def executeShellSingle(self, ):
-		pass
+	def executeShellSingle(self, command):
+		'''
+			TODO:
+				sanity check
+		'''
+		proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+		proc_exit_code = proc.returncode
+		output, errors = proc.communicate()
+
+		if proc_exit_code != 0:
+			return False, errors, proc_exit_code
+
+		return True, output, proc_exit_code
+
+	def executeShellList(self):
+		for command in self.command_list:
+
+			# log the command being executed
+
+			success, reponse, proc_exit_code = self.executeShellSingle(command)
+			if not success:
+				# log the response and proc_exit_code
+				return False
+
+			# log the response and proc_exit_code
+
+			return True
