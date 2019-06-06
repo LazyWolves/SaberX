2bef51cf59ead39c7d9113c6bfcb51404550a3bd
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-06-06 13:26:31 +0530

    Add file trigger

---
+++ saberx/sabercore/triggers/filetrigger.py
@@ -0,0 +1,12 @@
+from triggerbase import TriggerBase
+
+class FileTrigger(TriggerBase):
+	def __init__(self, **kwargs):
+		TriggerBase.__init__(type=kwargs.get("type"), check=kwargs.get("check"), negate=kwargs.get("negate"))
+
+		if kwargs.get("regex"):
+			self.regex = kwargs.get("regex")
+		if kwargs.get("position"):
+			self.position = kwargs.get("position")
+		if kwargs.get("limit"):
+			self.limit = kwargs.get("limit")	
