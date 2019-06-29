a9c3e9eadb0cfd036483417f593c0d0bb42c2dbc
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-06-29 21:09:29 +0530

    Add class for tcp check

---
+++ saberx/sabercore/triggers/tcphandler.py
@@ -0,0 +1,14 @@
+import os
+import socket
+
+class TCPHandler:
+
+    @staticmethod
+    def check_tcp(**kwargs):
+        pass
+
+    def check_tcp_ssl(**kwargs):
+        pass
+
+    def  check_connection(**kwargs):
+        pass
