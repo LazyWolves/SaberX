77ef651b2b55f8b80eeff86ce69a10be6ca89312
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-06-29 21:18:43 +0530

    Add tcp check non ssl

--- saberx/sabercore/triggers/tcphandler.py
+++ saberx/sabercore/triggers/tcphandler.py
@@ -5,7 +5,18 @@ class TCPHandler:
 
     @staticmethod
     def check_tcp(**kwargs):
-        pass
+
+        host = kwargs.get("host")
+        port = kwargs.get("port")
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect((host, int(port)))
+            sock.shutdown(2)
+            return True
+        except socket.error as err:
+            return False
+
 
     def check_tcp_ssl(**kwargs):
         pass
