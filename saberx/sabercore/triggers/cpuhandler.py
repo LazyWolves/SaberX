8266b458d7a778036e96d0902defa2b6950feb48
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-07-04 00:55:10 +0530

    Create CPU handler

---
+++ saberx/sabercore/triggers/cpuhandler.py
@@ -0,0 +1,30 @@
+import os
+import psutil
+
+class CPUHandler:
+
+    @staticmethod
+    def __operate(current, count, operator):
+        return {
+            '=': lambda current, count: current == count,
+			'<': lambda current, count: current < count,
+			'>': lambda current, count: current > count,
+			'<=': lambda current, count: current <= count,
+			'>=': lambda current, count: current >= count
+		}.get(operator)(current, count)
+
+    @staticmethod
+    def get_loadavg(**kwargs):
+
+        thresholds = kwargs.get("thresholds")
+        operation = kwargs.get("operation")
+
+        loadavg = os.getloadavg()
+
+        final_result = True
+        index = 0
+
+        for value in thresholds:
+            final_result = final_result and CPUHandler.__operate(loadavg[index], value, operation)
+        
+        return final_result, None
