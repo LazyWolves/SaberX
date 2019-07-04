e3012d4d0ca33a34c0cf6cfaa309b2d0c68ef3d0
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-07-05 00:24:46 +0530

    Add saniy check method

--- saberx/sabercore/triggers/cputrigger.py
+++ saberx/sabercore/triggers/cputrigger.py
@@ -1,7 +1,7 @@
 from triggerbase import TriggerBase
 from cpuhandler import TCPHandler
 
-class CPUTrigger(TriggerBase):pathpatpathhpath
+class CPUTrigger(TriggerBase):
 
     def __init__(self, **kwargs):
         TriggerBase.__init__(type=kwargs.get("type"), check=kwargs.get("check"), negate=kwargs.get("negate"))
@@ -13,3 +13,44 @@ class CPUTrigger(TriggerBase):pathpatpathhpath
             self.threshold = kwargs.get("threshold")
 
         self.valid_operations = ["=", "<", ">", "<=", ">="]
+        self.valid_checks = ["loadavg"]
+
+    def  fire_trigger(self):
+        pass
+
+    def sanitise(self):
+        if not self.operation:
+
+            '''
+                Log error
+            '''
+            return False
+
+        if self.operation not in self.valid_operations:
+
+            '''
+                Log error
+            '''
+            return False
+
+        if not self.check:
+
+            '''
+                Log error
+            '''
+            return False
+
+        if not self.check in self.valid_checks:
+
+            '''
+                Log error
+            '''
+            return False
+
+        for val in self.threshold:
+            if val != "-" and type(val) != float:
+
+                '''
+                    Log error
+                '''
+                return False
