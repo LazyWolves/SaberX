b20e403fef897a2cc992232477ed001175ff0f78
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-07-05 20:46:33 +0530

    Add Updated CPU trigger

--- saberx/sabercore/triggers/cputrigger.py
+++ saberx/sabercore/triggers/cputrigger.py
@@ -1,56 +1,60 @@
 from triggerbase import TriggerBase
-from cpuhandler import TCPHandler
+from cpuhandler import CPUHandler
 
 class CPUTrigger(TriggerBase):
 
-    def __init__(self, **kwargs):
-        TriggerBase.__init__(type=kwargs.get("type"), check=kwargs.get("check"), negate=kwargs.get("negate"))
+	def __init__(self, **kwargs):
+		TriggerBase.__init__(type=kwargs.get("type"), check=kwargs.get("check"), negate=kwargs.get("negate"))
 
-        if kwargs.get("operation"):
-            self.operation = kwargs.get("operation")
+		if kwargs.get("operation"):
+			self.operation = kwargs.get("operation")
 
-        if kwargs.get("threshold"):
-            self.threshold = kwargs.get("threshold")
+		if kwargs.get("threshold"):
+			self.threshold = kwargs.get("threshold")
 
-        self.valid_operations = ["=", "<", ">", "<=", ">="]
-        self.valid_checks = ["loadavg"]
+		self.valid_operations = ["=", "<", ">", "<=", ">="]
+		self.valid_checks = ["loadavg"]
 
-    def  fire_trigger(self):
-        pass
+	def  fire_trigger(self):
+		if not self.sanitise():
+			return False, "INVALID_ARGUMENTS"
 
-    def sanitise(self):
-        if not self.operation:
+		triggered, error = CPUHandler.check_loadavg(operation=self.operation, thresholds=self.threshold)
+		return self.eval_nagate(triggered, error)
 
-            '''
-                Log error
-            '''
-            return False
+	def sanitise(self):
+		if not self.operation:
 
-        if self.operation not in self.valid_operations:
+			'''
+				Log error
+			'''
+			return False
 
-            '''
-                Log error
-            '''
-            return False
+		if self.operation not in self.valid_operations:
 
-        if not self.check:
+			'''
+				Log error
+			'''
+			return False
 
-            '''
-                Log error
-            '''
-            return False
+		if not self.check:
 
-        if not self.check in self.valid_checks:
+			'''
+				Log error
+			'''
+			return False
 
-            '''
-                Log error
-            '''
-            return False
+		if not self.check in self.valid_checks:
 
-        for val in self.threshold:
-            if val != "-" and type(val) != float:
+			'''
+				Log error
+			'''
+			return False
 
-                '''
-                    Log error
-                '''
-                return False
+		for val in self.threshold:
+			if val != "-" and type(val) != float:
+
+				'''
+					Log error
+				'''
+				return False
