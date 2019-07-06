a3ef0dc93dc1029b3f2dbc78eeef125875288c54
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-07-06 23:52:13 +0530

    Populate memory handler

--- saberx/sabercore/triggers/memoryhandler.py
+++ saberx/sabercore/triggers/memoryhandler.py
@@ -2,4 +2,48 @@ import os
 import psutil
 
 class MemoryHandler:
-    pass
+
+    @staticmethod
+    def __operate(current, count, operator):
+        return {
+            '=': lambda current, count: current == count,
+			'<': lambda current, count: current < count,
+			'>': lambda current, count: current > count,
+			'<=': lambda current, count: current <= count,
+			'>=': lambda current, count: current >= count
+		}.get(operator)(current, count)
+
+    @staticmethod
+    def get_mem_type(check):
+
+        MEM_TYPES = {
+            "virtual": psutil.virtual_memory,
+            "swap": psutil.swap_memory
+        }
+
+        return MEM_TYPES.get(check)
+
+    @staticmethod
+    def get_attr_val(metrics, attr):
+        return {
+            "used": metrics.used,
+            "available": metrics.available,
+            "free": metrics.free
+        }.get(attr)
+
+    @staticmethod
+    def check_mem(**kwargs):
+
+        check_type = kwargs.get("type")
+        attr = kwargs.get("attr")
+        threshold = kwargs.get("threshold")
+        operator = kwargs.get("operator")
+
+        check = MemoryHandler.get_mem_type(check_type)
+
+        metrics = check()
+        attr_val =  MemoryHandler.get_attr_val(metrics, attr)
+
+        check_result = MemoryHandler.__operate(attr_val, threshold, operator)
+
+        return check_result
