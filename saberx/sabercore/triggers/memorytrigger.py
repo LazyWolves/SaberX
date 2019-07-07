722c917672cd7c8c4dd1dfd4e5f8195d7f0cc530
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-07-07 13:33:20 +0530

    Add memory trigger initialiser

--- saberx/sabercore/triggers/memorytrigger.py
+++ saberx/sabercore/triggers/memorytrigger.py
@@ -2,5 +2,20 @@ from triggerbase import TriggerBase
 from memoryhandler import MemoryHandler
 
 class MemoryTrigger(TriggerBase):
-    pass
+	def __init__(self, **kwargs):
+		TriggerBase.__init__(type=kwargs.get("type"), check=kwargs.get("check"), negate=kwargs.get("negate"))
 
+		if kwargs.get("attr"):
+			self.regex = kwargs.get("attr")
+		if kwargs.get("operation"):
+			self.operation = kwargs.get("operation")
+
+		self.valid_checks = ["virtual", "swap"]
+		self.valid_attrs = ["used", "available", "free"]
+		self.valid_operations = ["=", "<", ">", "<=", ">="]
+
+	def fire_trigger(self):
+		pass
+
+	def sanitise(self):
+		pass
