36667f2ebe0feee76764de9548250789f47574e9
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-08-11 11:45:33 +0530

    Add group execution method

--- saberx/executers/groupexecuter.py
+++ saberx/executers/groupexecuter.py
@@ -4,4 +4,23 @@ class GroupExecuter(object):
     
     @staticmethod
     def execute_group(group):
-        pass
+        if not GroupExecuter.sanitize(group):
+            return False
+
+        group_name = group.get("group_name")
+        actions = group.get("actions")
+
+        for action in actions:
+            success = ActionExecuter.execute_action(action)
+
+            if not success:
+                '''
+                    Log which action failed
+                '''
+                return False
+
+        return True
+
+    @staticmethod
+    def sanitize(group):
+        return True
