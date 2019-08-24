22d314a48fe240b5b068368e26c1a8553ff26747
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-08-25 01:35:38 +0530

    Add driver.py

---
+++ saberx/driver/driver.py
@@ -0,0 +1,8 @@
+from saberx.executers.threaddriver import ThreadExecuter
+from saberx.actionparser.actionextractor import ActionExtractor
+import time
+
+CONFIG_FILE = "saberx.config"
+
+def drive():
+    pass
