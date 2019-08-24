36c929370998dc05a692d5cfd13c9d2682604f01
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-08-24 23:28:56 +0530

    Add method to release lock

--- saberx/executers/threaddriver.py
+++ saberx/executers/threaddriver.py
@@ -1,6 +1,6 @@
 from saberx.executers.groupexecuter import GroupExecuter
 import threading
-import
+import os
 
 class ThreadExecuter:
     def __init__(self, **kwargs):
@@ -12,6 +12,14 @@ class ThreadExecuter:
             if not os.path.exists("/run/saberx/saberx.lock"):
                 with open("/run/saberx/saberx.lock", "w") as lock_file:
                     lock_file.write(os.getpid())
+            return True
+        except Exception as e:
+            return False
+
+    def __release_lock():
+        try:
+            if os.path.exists("/run/saberx/saberx.lock"):
+                os.unlink("/run/saberx/saberx.lock")
             return True
         except Exception as e:
             return False
