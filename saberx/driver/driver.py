3633b2f28a66ff9b8b7c196efb9480d3e4b9e46b
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-08-25 11:59:35 +0530

    Add argument parser

--- saberx/driver/driver.py
+++ saberx/driver/driver.py
@@ -1,8 +1,17 @@
 from saberx.executers.threaddriver import ThreadExecuter
 from saberx.actionparser.actionextractor import ActionExtractor
 import time
+import optparse
+import os
 
 CONFIG_FILE = "saberx.config"
 
 def drive():
-    pass
+    # parse args
+
+    parser = optparse.OptionParser()
+    parser.add_option('-f', action="store", dest="config", help="Config file.")
+    options, args = parser.parse_args()
+
+    if options.config:
+        CONFIG_FILE = options.config
