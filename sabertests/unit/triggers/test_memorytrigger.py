fcbfa8e57f522407b3a811684c5ec389803daad2
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-11-09 15:14:00 +0530

    Add test for memory trigger

---
+++ sabertests/unit/triggers/test_memorytrigger.py
@@ -0,0 +1,30 @@
+from saberx.sabercore.triggers.memorytrigger import MemoryTrigger
+
+class TestMemoryTrigger:
+    
+    def test_memory_trigger(self):
+        memoryTrigger = MemoryTrigger(
+                        type="MEMORY_TRIGGER",
+                        attr="used",
+                        threshold=10.0,
+                        operation='>',
+                        check="virtual"
+                    )
+        
+        triggered, error = memoryTrigger.fire_trigger()
+
+        assert triggered == True
+        assert error == None
+
+        memoryTrigger = MemoryTrigger(
+                        type="MEMORY_TRIGGER",
+                        attr="used",
+                        threshold=10.0,
+                        operation='>',
+                        check="swap"
+                    )
+        
+        triggered, error = memoryTrigger.fire_trigger()
+
+        assert triggered == True
+        assert error == None
