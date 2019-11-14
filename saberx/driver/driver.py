af2ef405e213c2f555d31d6f90457d21e4e3916f
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-11-15 00:36:28 +0530

    Add module doc for driver

--- saberx/driver/driver.py
+++ saberx/driver/driver.py
@@ -1,3 +1,9 @@
+"""
+.. module:: driver
+   :synopsis: Main entry point for Saberx
+
+"""
+
 from saberx.executers.threaddriver import ThreadExecuter
 from saberx.actionparser.actionextractor import ActionExtractor
 from configparser import SafeConfigParser
