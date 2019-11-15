d0048e3d5ee2adedac072071e3eaf16d248a6641
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-11-16 01:17:31 +0530

    Add doc for cpu trigger

--- saberx/sabercore/triggers/cputrigger.py
+++ saberx/sabercore/triggers/cputrigger.py
@@ -1,9 +1,23 @@
+"""
+.. module:: driver
+   :synopsis: Module for firing CPU trigger.
+"""
+
 from .triggerbase import TriggerBase
 from .cpuhandler import CPUHandler
 
 class CPUTrigger(TriggerBase):
 
+	"""
+		**Class for creating CPU trigger**
+	"""
+
 	def __init__(self, **kwargs):
+
+		"""
+			**Method for initialing CPU trigger**
+
+		"""
 		super(CPUTrigger, self).__init__(type=kwargs.get("type"), check=kwargs.get("check"), negate=kwargs.get("negate"))
 
 		self.operation = kwargs.get("operation", ">")
@@ -15,6 +29,16 @@ class CPUTrigger(TriggerBase):
 		self.valid_checks = ["loadaverage"]
 
 	def  fire_trigger(self):
+
+		"""
+			**Method to fire the trigger**
+
+			This method first sanitises the parameters, calls cpu handler
+			to evaluate the trigger conditions and returns trigger status
+
+			Returns:
+				bool : Trigger fired or not
+		"""
 		if not self.sanitise():
 			return False, "INVALID_ARGUMENTS"
 
@@ -22,6 +46,13 @@ class CPUTrigger(TriggerBase):
 		return self.eval_negate(triggered, error)
 
 	def sanitise(self):
+
+		"""
+			**Method to check validity of the params**
+
+			Returns:
+				bool : params are proper or not
+		"""
 		if not self.operation:
 
 			'''
