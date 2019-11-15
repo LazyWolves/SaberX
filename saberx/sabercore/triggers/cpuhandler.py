2c128320ce521956137f67908bc704d19a327382
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-11-16 01:38:26 +0530

    Add doc cpu handler

--- saberx/sabercore/triggers/cpuhandler.py
+++ saberx/sabercore/triggers/cpuhandler.py
@@ -1,9 +1,33 @@
+"""
+.. module:: cpuhandler
+   :synopsis: Module for evaluating trigger conditions.
+"""
+
 import os
 
 class CPUHandler:
 
+    """
+        Class for evaluatingcputrigger params
+    """
+
     @staticmethod
     def __operate(current, count, operator):
+
+        """
+            **Method for comparing given count with current cpu load everage.**
+
+            Thos metho takes the current value and the threshold for load average
+            and performs the desired operation.
+
+            Args:
+                current (Integer) : Current value of load average
+                count (Integer) : Given threshold
+                operator (string) : Given operator
+
+            Returns:
+                bool : Opeartion returned true or false
+        """
         return {
             '=': lambda current, count: current == count,
 			'<': lambda current, count: current < count,
@@ -15,6 +39,19 @@ class CPUHandler:
     @staticmethod
     def check_loadavg(**kwargs):
 
+        """
+            **Method to check loadaverage**
+
+            This method evaluates the trigger param and descied whether the trigger
+            is fired or not.
+
+            Args:
+                kwargs (object) : Object containing thresholds and operation
+
+            Returns:
+                bool, string : status of the trigger and error string if any
+        """
+
         thresholds = kwargs.get("thresholds")
         operation = kwargs.get("operation")
 
@@ -23,6 +60,7 @@ class CPUHandler:
         final_result = True
         index = 0
 
+        # evaluate the condition for each component of load average
         for value in thresholds:
             final_result = final_result and CPUHandler.__operate(loadavg[index], value, operation)
             index += 1
