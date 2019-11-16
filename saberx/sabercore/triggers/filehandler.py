2f9bd12f1d14d9089f120fd9b36828db69259cd0
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-11-16 15:27:45 +0530

    Add doc for file trigger

--- saberx/sabercore/triggers/filehandler.py
+++ saberx/sabercore/triggers/filehandler.py
@@ -1,3 +1,8 @@
+"""
+.. module:: filehandler
+   :synopsis: Module to evaluate file trigger
+"""
+
 import os
 from itertools import islice
 import re
@@ -5,8 +10,25 @@ import traceback
 
 class FileHandler:
 
+    """
+        ** Class containing file handling methods **
+    """
+
     @staticmethod
     def is_present(path):
+
+        """
+            **Method for checking if the given file exists**
+
+            This method checks whether the given path is valid or not.
+
+            Args:
+                path (string): path to the file resource
+
+            Returns:
+                bool, string: status, error if any
+
+        """
         if not os.path.exists(path):
             return False, None
 
@@ -17,6 +39,16 @@ class FileHandler:
 
     @staticmethod
     def is_empty(path):
+
+        """
+            **Method for checking if given file is empty**
+
+            Args:
+                path (string): path to the file resource
+
+            Returns:
+                bool, string: status, error if any
+        """
         if os.stat(path).st_size == 0:
             return True, None
 
@@ -24,6 +56,21 @@ class FileHandler:
 
     @staticmethod
     def read_from_head(path, regex, limit):
+
+        """
+            **Method to read a file from head and see if pattern exists**
+
+            This method reads a given file and checks if the given pattern
+            exists in the top 'n' lines where n is given as 'limits'.
+
+            Args:
+                path (string): path to the file resource
+                regex (string): string representing the pattern to search for
+                limit (Integer): Number of lines to query
+
+            Returns:
+                bool, string: status , error
+        """
         pattern = re.compile(regex)
         with open(path) as file:
             for row in islice(file, 0, limit):
@@ -35,6 +82,21 @@ class FileHandler:
 
     @staticmethod
     def read_from_tail(path, regex, lines):
+
+        """
+            **Method to read a file from end and see if pattern exists**
+
+            This method reads a given file and checks if the given pattern
+            exists in the last 'n' lines where n is given as 'lines'.
+
+            Args:
+                path (string): path to the file resource
+                regex (string): string representing the pattern to search for
+                limit (Integer): Number of lines to query
+
+            Returns:
+                bool, string: status , error
+        """
         pattern = re.compile(regex)
 
         lines_found = []
@@ -64,16 +126,31 @@ class FileHandler:
 
     @staticmethod
     def search_keyword(**kwargs):
+
+        """
+            **Method to execute the file operation**
+
+            Method to perform the reuired file operations to
+            execute the trigger
+
+            Args:
+                kwargs (dict): Dict containing path, pattern, limit and position to read the file from
+
+            Returns:
+                bool, string: status, error if any
+        """
         path = kwargs.get("path")
         regex = kwargs.get("regex")
         limit = kwargs.get("limit")
         position = kwargs.get("position")
 
+        # First of all, check if the file resource exists
         is_present, error = FileHandler.is_present(path)
 
         if not is_present:
             return False, "FILE_DOES_NOT_EXISTS"
 
+        # Next, check if the file is empty
         is_empty, error = FileHandler.is_empty(path)
 
         if is_empty:
