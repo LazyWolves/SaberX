e06d900dffcfbf6184b56ffb71004ae55738f1a3
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-11-16 14:07:29 +0530

    Add doc for file trigger

--- saberx/sabercore/triggers/filetrigger.py
+++ saberx/sabercore/triggers/filetrigger.py
@@ -1,8 +1,22 @@
+"""
+.. module:: filetrigger
+   :synopsis: Module for firing file trigger.
+"""
+
 from .triggerbase import TriggerBase
 from .filehandler import FileHandler
 
 class FileTrigger(TriggerBase):
+
+	"""
+		**Class for creating file trigger**
+	"""
 	def __init__(self, **kwargs):
+
+		"""
+			**Method for initialing File trigger**
+
+		"""
 		super(FileTrigger, self).__init__(type=kwargs.get("type"), check=kwargs.get("check"), negate=kwargs.get("negate"))
 
 		if kwargs.get("regex"):
@@ -17,6 +31,16 @@ class FileTrigger(TriggerBase):
 		self.valid_positions = ["head", "tail"]
 
 	def fire_trigger(self):
+
+		"""
+			**Method to fire the trigger**
+
+			This method first sanitises the parameters, calls file handler
+			to evaluate the trigger conditions and returns trigger status
+
+			Returns:
+				bool : Trigger fired or not
+		"""
 		if not self.sanitise():
 			return False, "IMPROPER_ARGUMENTS"
 
