01c7bbb550cc17f0f6b4cbaff503643863cda1fe
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-11-16 11:10:41 +0530

    Add doc process handler

--- saberx/sabercore/triggers/processhandler.py
+++ saberx/sabercore/triggers/processhandler.py
@@ -1,17 +1,43 @@
+"""
+.. module:: processhandler
+   :synopsis: Module for evaluating process trigger.
+"""
+
 import psutil
 import subprocess
 import re
 
 class ProcessHandler:
 
+	"""
+		**Class for performing process trigger operation
+	"""
+
 	@staticmethod
 	def get_name_count(regex):
+
+		"""
+			**Method for getting count of process**
+
+			This method returns the count of processes whose
+			name matches the given regex
+
+			Args:
+				regex (string): String containing regex for filtering process names
+
+			Returns:
+				bool, Integer, String : status, count, error if any
+		"""
 		pattern = re.compile(regex)
 		count = 0
 
 		try:
+
+			# loop over all the processes fetched via psutil
 			for proc in psutil.process_iter(attrs=['name']):
 				proc_name = proc.info['name']
+
+				# filter them using the regex
 				if pattern.search(proc_name):
 					count += 1
 
@@ -25,6 +51,22 @@ class ProcessHandler:
 
 	@staticmethod
 	def get_cmdline_count(regex):
+
+		"""
+			**Method for getting count of process**
+
+			This method returns the count of processes whose
+			cmdline matches the given regex
+
+			Args:
+				regex (string): String containing regex for filtering process cmdline
+
+			Returns:
+				bool, Integer, String : status, count, error if any
+		"""
+
+		# command for getting all processes in the system filtered
+		# by the process name
 		command = 'ps aux | grep -E "{}" | grep -v grep | wc -l'.format(regex)
 		proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 		output, errors = proc.communicate()
@@ -38,6 +80,19 @@ class ProcessHandler:
 
 	@staticmethod
 	def check_name(regex):
+
+		"""
+			**Method for checking if a process exists by name**
+
+			This method checks if there is any process whose name matches the
+			given pattern
+
+			Args:
+				regex (string): String containing regex
+
+			Returns:
+				bool, Integer, String : status, count, error if any
+		"""
 		response, count, error = ProcessHandler.get_name_count(regex)
 
 		if error:
@@ -50,6 +105,18 @@ class ProcessHandler:
 
 	@staticmethod
 	def check_cmdline(regex):
+		"""
+			**Method for checking if a process exists by cmdline text**
+
+			This method checks if there is any process whose cmdline arg matches the
+			given pattern
+
+			Args:
+				regex (string): String containing regex
+
+			Returns:
+				bool, Integer, String : status, count, error if any
+		"""
 		response, count, error = ProcessHandler.get_cmdline_count(regex)
 
 		if error:
@@ -62,6 +129,18 @@ class ProcessHandler:
 
 	@staticmethod
 	def __operate(current, count, operator):
+
+		"""
+			**Method to evaluate the required operation**
+
+			Args:
+				current (Integer) : Current number of processes filtered by the given regex
+				count (Integer) : Desired threshold
+				operator (String) : The operation to be performed
+
+			Returns:
+				bool: Result of the operattion
+		"""
 		return {
 			'=': lambda current, count: current == count,
 			'<': lambda current, count: current < count,
@@ -72,6 +151,18 @@ class ProcessHandler:
 
 	@staticmethod
 	def check_name_count(regex, count, operator):
+
+		"""
+			**Method to get the Number of regex filtered processes and perform the deired operation**
+
+			Args:
+				regex (string): Regex to filter processes
+				count (string): Threshold
+				operation (string): Desired operation
+
+			Returns:
+				bool, string : Result, error if any
+		"""
 		response, current, error = ProcessHandler.get_name_count(regex)
 
 		if error:
@@ -83,6 +174,28 @@ class ProcessHandler:
 
 	@staticmethod
 	def check_cmdline_count(regex, count, operator):
+
+		"""
+			**Method to get the Number of regex filtered processes by cmd and perform the deired operation**
+
+			Args:
+				regex (string): Regex to filter p"""
+			**Method to get the Number of regex filtered processes and perform the deired operation**
+
+			Args:
+				regex (string): Regex to filter processes
+				count (string): Threshold
+				operation (string): Desired operation
+
+			Returns:
+				bool, string : Result, error if any
+		"""rocesses
+				count (string): Threshold
+				operation (string): Desired operation
+
+			Returns:
+				bool, string : Result, error if any
+		"""
 		response, current, error = ProcessHandler.get_cmdline_count(regex)
 
 		if error:
