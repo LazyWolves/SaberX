9c495c3e721b0960d77469dc4d534dc922ca7562
Thompson, Brian <brian.thompson@mastercard.com>
2020-01-28 03:59:47 -0600

    Fix flake8 e111 errors

--- setup.py
+++ setup.py
@@ -12,41 +12,45 @@ SAMPLE_CONFIG_DIR = "etc"
 CONFIG_FILE = "saberx.conf"
 TEMPLATE_FILE = "saberx.yaml.example"
 
+
 def pre_setup():
-  if not os.path.exists(LOG_DIR):
-    os.mkdir(LOG_DIR)
+    if not os.path.exists(LOG_DIR):
+        os.mkdir(LOG_DIR)
 
-  if not os.path.exists(CONFIG_DIR):
-    os.mkdir(CONFIG_DIR)
+    if not os.path.exists(CONFIG_DIR):
+        os.mkdir(CONFIG_DIR)
 
-  base_path = os.path.dirname(os.path.realpath(__file__))
+    base_path = os.path.dirname(os.path.realpath(__file__))
 
-  source_config = os.path.join(base_path, SAMPLE_CONFIG_DIR, CONFIG_FILE)
-  source_template = os.path.join(base_path, SAMPLE_CONFIG_DIR, TEMPLATE_FILE)
+    source_config = os.path.join(base_path, SAMPLE_CONFIG_DIR, CONFIG_FILE)
+    source_template = os.path.join(base_path, SAMPLE_CONFIG_DIR, TEMPLATE_FILE)
 
-  shutil.copy(source_config, os.path.join(CONFIG_DIR, CONFIG_FILE))
-  shutil.copy(source_template, os.path.join(CONFIG_DIR, TEMPLATE_FILE))
+    shutil.copy(source_config, os.path.join(CONFIG_DIR, CONFIG_FILE))
+    shutil.copy(source_template, os.path.join(CONFIG_DIR, TEMPLATE_FILE))
+
 
 def readme():
     return "Saberx"
-    #with open('README.rst') as f:
-        #return f.read()
+    # with open('README.rst') as f:
+    # return f.read()
+
 
 def get_dependencies():
-  with open("requirements.txt") as req:
-    lines = req.readlines()
-    lines = [line.strip() for line in lines]
-    return lines
+    with open("requirements.txt") as req:
+        lines = req.readlines()
+        lines = [line.strip() for line in lines]
+        return lines
+
 
 setup(name='saberx-1.0',
-      version='1.0.1',
+      version='1.0.2',
       description='Self healing monitoring system',
       long_description=readme(),
       classifiers=[
-        'Development Status :: 1.0',
-        'License :: GPL',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Automation :: Systems :: Devops :: monitoring :: selfhealing',
+          'Development Status :: 1.0',
+          'License :: GPL',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Automation :: Systems :: Devops :: monitoring :: selfhealing',
       ],
       keywords='automation devops systems monitoring selfhealing',
       url='https://github.com/djmgit/Saberx',
@@ -62,4 +66,4 @@ setup(name='saberx-1.0',
       zip_safe=False)
 
 if __name__ == "__main__":
-  pre_setup()
+    pre_setup()
